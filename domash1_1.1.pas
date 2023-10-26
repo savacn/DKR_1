534b74a875c997d6ecc01f6deecd64c77a47759d
Savely <112480298+savacn@users.noreply.github.com>
2023-10-26 13:00:03 +0300

    Add files via upload

---
+++ domash1_1.1.pas
@@ -0,0 +1,12 @@
+﻿program prod_dkr_1;
+var
+  x, y: real;
+begin
+  write('Введите х:  ');
+  readln(x);
+  if (x < -6) then y := x**(0.1*x)*ln(x)+ln(x)/x;
+  if (x < 0) then y:= cos(x)/x**3;
+  if (x < 4) then y:= x**2*x**2+89/94;
+  if (x >= 4) then y:= sin(x)/cos(x)-(-x);
+  writeln('y=', y:0:2);
+end.
---
+++ domash1_1.2.pas
@@ -0,0 +1,15 @@
+﻿program prod_dkr_1_2;
+var
+  x, y: real;
+begin
+  x := -8;
+  while x < 6.3 do
+  begin
+    if (x < -6) then y := x**(0.1*x)*ln(x)+ln(x)/x;
+    if (x < 0) then y:= cos(x)/x**3;
+    if (x < 4) then y:= x**2*x**2+89/94;
+    if (x >= 4) then y:= sin(x)/cos(x)-(-x);
+    writeln('x= ', x:0:1, '     ', 'y= ', y:0:2);
+    x := x + 0.3;
+  end;
+end.
